63ad770df8903d21591397efc6ef021a434f231e
Alexis <pingrisalexis@gmail.com>
2022-04-07 16:54:57 +0200

    add router logic

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Logo from '../../assets/argentBankLogo.png';
 import styles from './Header.module.scss';
-
+import {Link} from 'react-router-dom';
 /**
  * @name Header
  * @description This is the header component.
@@ -12,30 +12,30 @@ const Header = () => {
     return (
         <header className={styles.header_container}>
                 <nav className={styles.header_nav}>
-                    <a className={styles.header_nav_logo} href="./index.html">
+                    <Link to="/" className={styles.header_nav_logo}>
                         <img
                             className={styles.header_nav_logo_image}
                             src={Logo}
                             alt="Argent Bank Logo"
                         />
                         <h1 className="sr-only">Argent Bank</h1>
-                    </a>
+                    </Link>
                     <div>
-                        <a className={styles.header_nav_item} href="./sign-in.html">
+                        <Link to="/Login" className={styles.header_nav_item}>
                             <i className="fa fa-user-circle"/>
                             Sign In
-                        </a>
+                        </Link>
                     </div>
-                    <div>
-                        <a className={styles.header_nav_item} href="./user.html">
-                            <i className="fa fa-user-circle" />
-                            Tony
-                        </a>
-                        <a className={styles.header_nav_item} href="./index.html">
-                            <i className="fa fa-sign-out" />
-                            Sign Out
-                        </a>
-                    </div>
+                    {/*<div>*/}
+                    {/*    <a className={styles.header_nav_item} href="./user.html">*/}
+                    {/*        <i className="fa fa-user-circle" />*/}
+                    {/*        Tony*/}
+                    {/*    </a>*/}
+                    {/*    <a className={styles.header_nav_item} href="./index.html">*/}
+                    {/*        <i className="fa fa-sign-out" />*/}
+                    {/*        Sign Out*/}
+                    {/*    </a>*/}
+                    {/*</div>*/}
                 </nav>
         </header>
     )
