75f95f4ff6bcce3d54cef24c62e9012010220d88
Alexis <pingrisalexis@gmail.com>
2022-04-07 16:57:16 +0200

    Routes logics

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
- export { default as  Header } from './header/Header';
-export { default as Footer } from './footer/Footer'
+export { default as  Header } from './header/Header';
+export { default as Footer } from './footer/Footer';
+export { default as FeatureItem } from './feature-item/FeatureItem';
+export { default as HeroItem } from './hero-item/HeroItem';
+export { default as SignInForm } from './sign-in-form/SignInForm';
