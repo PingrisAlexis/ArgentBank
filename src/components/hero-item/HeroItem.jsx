c0be789c3b62ed9239f29fb6aadb7d8fa3df2f7e
Alexis <pingrisalexis@gmail.com>
2022-04-07 16:55:09 +0200

    Create heros items

---
+++ src/components/hero-item/HeroItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './HeroItem.module.scss';
+
+const HeroItem = ({heroContent}) => {
+    return (
+        <p className={styles.subtitle}>{heroContent.description}</p>
+    )
+}
+
+export default HeroItem;
