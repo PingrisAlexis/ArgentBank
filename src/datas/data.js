98c72e3eff8b24dc2aa9e195a6615f4e8968616e
Alexis <pingrisalexis@gmail.com>
2022-04-07 16:56:51 +0200

    JSON

---
+++ src/datas/data.js
@@ -0,0 +1,42 @@
+import ChatIcon from '../assets/icon-chat.png';
+import MoneyIcon from '../assets/icon-money.png';
+import SecurityIcon from '../assets/icon-security.png';
+
+export const FEATURES_CARDS_CONTENT = [
+    {
+        id: 'feature01',
+        title: 'You are our #1 priority',
+        description: 'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
+        imgSrc: ChatIcon,
+        imgAlt: 'Chat icon'
+    },
+    {
+        id: 'feature02',
+        title: 'More savings means higher rates',
+        description: 'The more you save with us, the higher your interest rate will be!',
+        imgSrc: MoneyIcon,
+        imgAlt: 'Money icon'
+    },
+    {
+        id: 'feature03',
+        title: 'Security you can trust',
+        description: 'We use top of the line encryption to make sure your data and money is always safe.',
+        imgSrc: SecurityIcon,
+        imgAlt: 'Security icon'
+    }
+]
+
+export const HEROS_ITEMS_CONTENT = [
+    {
+        id: 'hero01',
+        description: 'No fees.'
+    },
+    {
+        id: 'hero02',
+        description: 'No minimum deposit.'
+    },
+    {
+        id: 'hero3',
+        description: 'High interest rates.'
+    }
+]
