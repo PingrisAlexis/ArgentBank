fffc5333bd28f0c2537bf82da8ddd73ed7d4444c
Alexis <pingrisalexis@gmail.com>
2022-04-13 10:20:05 +0200

    import / export logics

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-export { default as  Header } from './header/Header';
-export { default as Footer } from './footer/Footer';
-export { default as FeatureItem } from './feature-item/FeatureItem';
-export { default as HeroItem } from './hero-item/HeroItem';
-export { default as SignInForm } from './sign-in-form/SignInForm';
+export { default as Header } from './header/Header'
+export { default as Footer } from './footer/Footer'
+export { default as FeatureItem } from './feature-item/FeatureItem'
+export { default as HeroItem } from './hero-item/HeroItem'
+export { default as SignInForm } from './sign-in-form/SignInForm'
