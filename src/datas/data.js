637bde3719bd3bf3b06c7ec5cbc1eeb7a2a7dcba
Alexis <pingrisalexis@gmail.com>
2022-04-13 10:21:09 +0200

    Clean + transactions cards

--- src/datas/data.js
+++ src/datas/data.js
@@ -1,42 +1,66 @@
-import ChatIcon from '../assets/icon-chat.png';
-import MoneyIcon from '../assets/icon-money.png';
-import SecurityIcon from '../assets/icon-security.png';
+import ChatIcon from '../assets/icon-chat.png'
+import MoneyIcon from '../assets/icon-money.png'
+import SecurityIcon from '../assets/icon-security.png'
 
 export const FEATURES_CARDS_CONTENT = [
     {
         id: 'feature01',
         title: 'You are our #1 priority',
-        description: 'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
+        description:
+            'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
         imgSrc: ChatIcon,
-        imgAlt: 'Chat icon'
+        imgAlt: 'Chat icon',
     },
     {
         id: 'feature02',
         title: 'More savings means higher rates',
-        description: 'The more you save with us, the higher your interest rate will be!',
+        description:
+            'The more you save with us, the higher your interest rate will be!',
         imgSrc: MoneyIcon,
-        imgAlt: 'Money icon'
+        imgAlt: 'Money icon',
     },
     {
         id: 'feature03',
         title: 'Security you can trust',
-        description: 'We use top of the line encryption to make sure your data and money is always safe.',
+        description:
+            'We use top of the line encryption to make sure your data and money is always safe.',
         imgSrc: SecurityIcon,
-        imgAlt: 'Security icon'
-    }
+        imgAlt: 'Security icon',
+    },
 ]
 
 export const HEROS_ITEMS_CONTENT = [
     {
         id: 'hero01',
-        description: 'No fees.'
+        description: 'No fees.',
     },
     {
         id: 'hero02',
-        description: 'No minimum deposit.'
+        description: 'No minimum deposit.',
     },
     {
         id: 'hero3',
-        description: 'High interest rates.'
-    }
+        description: 'High interest rates.',
+    },
+]
+
+export const TRANSACTIONS_CARDS_CONTENT = [
+    {
+        id: 'account01',
+        title: 'Argent Bank Checking (x8349)',
+        amount: '$2,082.79',
+        description: 'Available Balance',
+    },
+    {
+        id: 'account02',
+        title: 'Argent Bank Savings (x6712)',
+        amount: '$10,928.42',
+        description: 'Available Balance',
+    },
+    {
+        id: 'account03',
+        title: 'Argent Bank Credit Card (x8349)',
+        amount: '$184.30',
+        description: 'Current Balance',
+    },
 ]
