5c212908a0b2ea278220de9d09f9d35aa8a56f21
Alexis <pingrisalexis@gmail.com>
2022-04-13 10:21:20 +0200

    Routing logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import {Header, Footer} from './components';
-import {HomePage, LoginPage} from './views';
-import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
-import {useState} from 'react';
+import { Header, Footer } from './components'
+import { HomePage, LoginPage, ProfilePage } from './views'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
 function App() {
-
-
-
-
     return (
-    <div className="App">
-        <BrowserRouter>
-            <Header />
-            <Routes> s
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<LoginPage />}/>
-            </Routes>
-            <Footer />
-        </BrowserRouter>
-    </div>
-  );
+        <div className="App">
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/profile" element={<ProfilePage />} />
+                </Routes>
+                <Footer />
+            </BrowserRouter>
+        </div>
+    )
 }
 
-export default App;
+export default App
