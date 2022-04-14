eead37f28061fe2c4fac6a0d4008e6ad3fee5845
Alexis <pingrisalexis@gmail.com>
2022-04-14 19:11:37 +0200

    Create edit user name component

--- src/components/edit-user-name-form/EditUserNameForm.jsx
+++ src/components/edit-user-name-form/EditUserNameForm.jsx
@@ -1,25 +1,91 @@
-import React from 'react';
-import styles from './EditProfileForm.module.scss';
+import React, { useEffect, useState } from 'react';
+import styles from './EditUserNameForm.module.scss';
+import { useSelector } from 'react-redux';
+import { tokenSelector, userSelector } from '../../store/selectors/selectors';
+import { edit } from '../../store/actions/auth';
+import { useDispatch } from 'react-redux';
 
-const EditProfileForm = () => {
+/**
+ * @name EditUserNameForm
+ * @description This component will render the edit mode by click on edit button.
+ * @returns {JSX.Element}
+ */
+
+const EditUserNameForm = () => {
+    const user = useSelector(userSelector);
+    const token = useSelector(tokenSelector);
+    let dispatch = useDispatch();
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [editMode, setEditMode] = useState(false);
+
+    useEffect(() => {
+        setFirstName(user.firstName);
+        setLastName(user.lastName);
+    }, [user]);
+
+    const handleEdit = (event) => {
+        event.preventDefault();
+        setEditMode(false);
+        dispatch(edit(firstName, lastName, token));
+    };
+
     return (
-        <form>
-            <div className="preference">
-                <label htmlFor="firstname">
-                    <input
-                        type="text"
-                        name="firstname"
-                        placeholder={'azeaze'}
-                    />
-                </label>
-            </div>
-            <div className="preference">
-                <label htmlFor="lastname">
-                    <input type="text" name="lastname" placeholder={'azeaze'} />
-                </label>
-            </div>
-        </form>
+        <>
+            {editMode ? (
+                <form
+                    className={styles.edit_user_name_container}
+                    onSubmit={handleEdit}
+                >
+                    <div className={styles.edit_user_name_input_container}>
+                        <label htmlFor="firstname">
+                            <input
+                                minLength={2}
+                                maxLength={15}
+                                type="text"
+                                name="firstname"
+                                placeholder={firstName}
+                                onChange={(e) => setFirstName(e.target.value)}
+                            />
+                        </label>
+                        <label htmlFor="lastname">
+                            <input
+                                minLength={2}
+                                maxLength={15}
+                                type="text"
+                                name="lastname"
+                                placeholder={lastName}
+                                onChange={(e) => setLastName(e.target.value)}
+                            />
+                        </label>
+                    </div>
+                    <div className={styles.edit_user_name_button_container}>
+                        <button
+                            className={styles.edit_user_name_button}
+                            type="submit"
+                        >
+                            Save
+                        </button>
+                        <button
+                            className={styles.edit_user_name_button}
+                            onClick={() => setEditMode(false)}
+                        >
+                            Cancel
+                        </button>
+                    </div>
+                </form>
+            ) : (
+                <div className={styles.edit_user_name_button_container}>
+                    <button
+                        className={styles.edit_user_name_button}
+                        onClick={() => setEditMode(true)}
+                    >
+                        Edit
+                    </button>
+                </div>
+            )}
+        </>
     );
 };
 
-export default EditProfileForm;
+export default EditUserNameForm;
