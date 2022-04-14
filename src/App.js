4fd7a1792b4230e5792ef8efe0b1651a431be318
Alexis <pingrisalexis@gmail.com>
2022-04-14 19:11:21 +0200

    Add protected routes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import { Header, Footer } from './components'
-import { HomePage, LoginPage, ProfilePage } from './views'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
-import { useDispatch } from 'react-redux'
-import { useEffect } from 'react'
-import { remember } from './store/actions/auth'
+import { Header, Footer } from './components';
+import { HomePage, LoginPage, ProfilePage } from './views';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { remember } from './store/actions/auth';
+import { isLoggedInSelector } from './store/selectors/selectors';
+import Protected from './utils/protected-routes/ProtectedRoutes';
+import NotFound from './views/not-found/NotFound';
 
 function App() {
-    let dispatch = useDispatch()
+    let dispatch = useDispatch();
+    const isLoggedIn = useSelector(isLoggedInSelector);
+    const token = localStorage.getItem('token');
 
     useEffect(() => {
-        const token = localStorage.getItem('token')
         if (token) {
-            dispatch(remember(token))
+            dispatch(remember(token));
         }
-    }, [])
+    }, [token, dispatch]);
 
     return (
         <div className="App">
             <BrowserRouter>
                 <Header />
                 <Routes>
+                    <Route path="*" element={<NotFound />} />
                     <Route path="/" element={<HomePage />} />
                     <Route path="/login" element={<LoginPage />} />
-                    <Route path="/profile" element={<ProfilePage />} />
+                    <Route
+                        path="/profile"
+                        element={
+                            <Protected isLoggedIn={isLoggedIn}>
+                                <ProfilePage />
+                            </Protected>
+                        }
+                    />
                 </Routes>
                 <Footer />
             </BrowserRouter>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
