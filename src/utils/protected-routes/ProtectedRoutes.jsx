ffdc37bcac91a8f72be2862d994d1753a1be1b5f
Alexis <pingrisalexis@gmail.com>
2022-04-19 12:16:22 +0200

    Change file name

--- src/utils/protected-routes/ProtectedRoutes.jsx
+++ src/utils/protected-routes/ProtectedRoutes.jsx
@@ -1,8 +1,18 @@
 import { Navigate } from 'react-router-dom';
-const Protected = ({ isLoggedIn, children }) => {
+import TokenExpiration from '../token-expiration/TokenExpiration';
+/**
+ * @name ProtectedRoutes
+ * @description This utils will control if user is logged in or not, to render profile page or home page.
+ * @param {boolean} isLoggedIn
+ * @param {JSX.Element} children
+ * @returns {JSX.Element}
+ */
+
+const ProtectedRoutes = ({ isLoggedIn, children }) => {
+    console.log(TokenExpiration);
     if (!isLoggedIn) {
         return <Navigate to="/" replace />;
     }
     return children;
 };
-export default Protected;
+export default ProtectedRoutes;
