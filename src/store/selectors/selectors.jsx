2861db9b9602969987818017e593fc255122d7c9
Alexis <pingrisalexis@gmail.com>
2022-04-20 16:59:08 +0200

    Fix token problems

--- src/store/selectors/selectors.jsx
+++ src/store/selectors/selectors.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import {
+    decodeToken,
+    isTokenValid,
+} from '../../utils/token-validity/TokenValidity';
 
 export const tokenSelector = (state) => state.token;
 export const userSelector = (state) => state.user;
-export const errorSelector = (state) => state.error;
-export const isLoggedInSelector = (state) => state.isLoggedIn;
-export const isLoadingSelector = (state) => state.loading;
+export const isLoadingSelector = (state) => state.isLoading;
+export const isLoggedInSelector = (state) => {
+    if (state.token !== null && state.token !== undefined) {
+        return isTokenValid(decodeToken(state.token));
+    }
+    return false;
+};
