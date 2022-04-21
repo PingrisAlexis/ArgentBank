5de59d9d56a5d3d4407b689e8745b95db326ff69
Alexis <pingrisalexis@gmail.com>
2022-04-21 19:16:30 +0200

    Delete LOGIN_FAIL

--- src/store/actions/types.jsx
+++ src/store/actions/types.jsx
@@ -1,5 +1,4 @@
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT = 'LOGOUT';
 export const USER_PROFILE = 'USER_PROFILE';
 export const LOADING = 'LOADING';
